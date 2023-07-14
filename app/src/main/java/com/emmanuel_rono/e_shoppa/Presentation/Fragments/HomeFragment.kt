dccbcfca4944ef4825e3f04e5c1f2ba3df726d11
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-07-14 03:56:20 +0300

    Proceeding

--- app/src/main/java/com/emmanuel_rono/e_shoppa/Presentation/Fragments/HomeFragment.kt
+++ app/src/main/java/com/emmanuel_rono/e_shoppa/Presentation/Fragments/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.emmanuel_rono.e_shoppa.Presentation.Fragments
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
@@ -27,13 +30,14 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_home, container, false)
         binding = FragmentHomeBinding.bind(rootView)
+
         return rootView
     }
     @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         adapter= ProductAdapter(emptyList())
-        recyclerView=binding.homeRecyclerView
+        recyclerView = binding.homeRecyclerView
         recyclerView.adapter=adapter
         recyclerView.layoutManager=GridLayoutManager(requireContext(),2)
         //val products = getProductsFromApi() //
